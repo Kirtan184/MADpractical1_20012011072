528718acc5fdc8f65b050fc00ebc591530b65c3b
Kirtan Panchal <110648465+Kirtan184@users.noreply.github.com>
2022-08-23 23:39:10 +0530

    Add files via upload

---
+++ MADpractical1_20012011072/src/main/kotlin/practical1.kt
@@ -0,0 +1,28 @@
+fun main() {
+    var x = 22
+    println("Integer value: $x")
+
+    var y = 1.5
+    println("Float value: $y")
+
+    var ch = 'T'
+    println("Character value: $ch")
+
+    var s = "HMS"
+    println("String value: $s")
+
+    var b = false
+    println("Boolean value: $b")
+
+    var d = 96.36
+    println("Double value: $d")
+
+    var l = 338927847
+    println("Long value: $l")
+
+    var sh = -2
+    println("Short value: $sh")
+
+    var a = 127
+    println("Byte value: $a")
+}
